68702d2e46f7dbf1a3137e2609a6c266305c3f61
Diego Arthur <digo.arthur@hotmail.com>
2022-10-05 20:55:40 +0100

    add Scripts cap. 11

---
+++ scripts/cap11/codR11.3/add.cpp
@@ -0,0 +1,6 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+// [[Rcpp::export]]
+int add(int x, int y) {
+	return x + y;
+}
---
+++ scripts/cap11/codR11.3/codR11.3.r
@@ -0,0 +1,14 @@
+##################################
+# Scripts do R Basico            #
+# Colecao Estudando o Ambiente R #
+# Autores: Ben Deivide de Oliveira Batista e Diego Arthur Bispo Justino de Oliveira   #
+##################################
+# CODIGO R11.3
+##################################
+
+
+# Carregando o script add.cpp
+Rcpp::sourceCpp("add.cpp")
+# Chamando a funcao add
+add(5, 10)
+
